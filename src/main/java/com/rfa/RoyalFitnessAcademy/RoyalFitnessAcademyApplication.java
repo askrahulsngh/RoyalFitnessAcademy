521986b6470cc6e7031ae10c2c4f740c2bf8f5c5
askrahulsngh <askrahulsngh@gmail.com>
2019-04-21 17:28:01 +0530

    First Push

---
+++ src/main/java/com/rfa/RoyalFitnessAcademy/RoyalFitnessAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.rfa.RoyalFitnessAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyalFitnessAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoyalFitnessAcademyApplication.class, args);
+	}
+
+}
